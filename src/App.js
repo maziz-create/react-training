427d4cb736cc283e9f93294922c1c374e3a678ea
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-22 00:39:14 +0300

    added Header component into jsx, added some rules for jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
+    /*
+    kural=> bu return kısmında bir div olmadan proje ayağa kalkmaz.
+    çünkü kapsayıcı bir element şart. div kullanılmayadabilir.
+    "<>"
+    "</>" şeklinde fragment isimli şeyi de kullanabilirsin. yeter ki return altındaki her şeyi kapsayan en az tek bir şey olsun.
+    */
+
+    /*
+    kural => bazı özel keyword'ler var. bunlar jsx içinde yazılamaz. if,for(label for'undan bahsediyoruz. zaten kendisi
+     htmlFor olarak açıyor), class(className yazcan)
+    */
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header componentini çalıştırdık. eğer içinde
+       bir children yoksa eğer <Header></Header> olarak
+       kullanılmamalıymış.*/}
+      <Header />
+      <Header />
+      <Header />
+      {/* jsx nedir ? => burası bir javascript dökümanı ve içinde html yazabiliyoruz...
+      bunun sebebini jsx veriyor. bu yazdıklarımız birer html değil aslında. javascript fakat
+      arka tarafta html'e dönüştürülüyor. index.html'de root var, o roota gönderiliyor o da doma yolluyor muhtemelen.
+       */}
     </div>
   );
 }
