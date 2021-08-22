68f9ff0a4f11039dfdac0e1f60eea0d676c52b0f
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-22 18:39:59 +0300

    added components folder and added Counter.js into it

---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+// bilgi => indirilen react eklentisi sayesinde rfce + tab yapınca react otomatik olarak geliyor.
+import React from 'react'
+
+function Counter() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Counter
