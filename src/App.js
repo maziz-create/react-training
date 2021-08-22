112c1f34e5c408e6caeb7f2368feb18eb60236f3
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-22 17:08:51 +0300

    imported useState from 'react', adding name state as a variable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+// state tanımlıyoruz
+import { useState } from "react";
 
 function App() {
+  //State'i böyle tanımlıyoruz. const[stataName, functionOfChangeName] = useState(initialState);
+  const [name, setName] = useState("Mehmet Aziz");
+
   return (
     <div className="App">
       <h1>Merhaba!</h1>
