2739db00c402945644d1803bf40d3ff2c5be6eda
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-22 18:36:01 +0300

    Added 'counterapp' branch, Existing content deleted because will be added counter app

--- src/App.js
+++ src/App.js
@@ -1,60 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-// state tanımlıyoruz
-import { useState } from "react";
-/* 
-  state nedir ? 
-  uygulamamızda değerinin değişme potansiyeli yüksek olan şeyleri taşıyan javascript objesidir.
-  istediğimiz bir anda istediğimiz bir değeri değiştirebiliriz. mesela birisi mesaj attığında
-  sayfayı yenilemeden o mesajı görmek gibi.
-*/
 
-// not: herhangi bir state güncellendiği anda return işlemi baştan render edilir.
 function App() {
-  //State'i böyle tanımlıyoruz. const[stataName, functionOfChangeName] = useState(initialState);
-  const [name, setName] = useState("Mehmet Aziz");
-
-  //State'in initialValue ' sunu array olarak verip aşağıda map ederek yazdırabiliriz.
-  const [friends, setFriends] = useState(["Deniz", "Samet"]);
-
-  //State'in initialValue ' sunu obje olarak verebiliriz.
-  const [address, setAddress] = useState({ title: "Ankara", zip: 606 });
-
   return (
     <div className="App">
-      <h1>Merhaba, {name}!</h1>
-      <button onClick={() => setName("isim değiştirildi => Ahmet")}>Click</button>
-
-      {//key verme sebebimiz => buralarda map gibi şeyleri kullanman için her bi dive unique key vermek zorundasın
-        friends
-          .map((friend, index) => (
-            <div key={index}>{friend}</div>
-          ))
-      }
-      {/* 
-          friends dizisine yeni eleman ekledik. dikkat edilmesi gerekenler:
-          => dizinin initialState'i bir dizi olduğu için biz de dizi olarak verdik. 
-          Yoksa yukarıdaki mapleme işlemi sıkıntı çıkartır.
-          => önceki değerleri vermeyip direkt Onuru verirsek eski değerler gider.
-          Bunun için    ...eskiDizi  mevzusu var, biliyorsun.
-      */}
-      <button onClick={() => setFriends([...friends, ['Yeni Kişi => Onur']])}>Click</button>
-
-      <br />
-      <br />
-
-      {/* adresi render ettik ve butonla değiştirdik. */}
-      <h2>Address</h2>
-      <div>
-        {address.title} {address.zip}
-      </div>
-
-      <button onClick={() => setAddress({
-        title: "degistirilen adres",
-        zip: 111
-      })}>Click</button>
-
-
     </div>
   );
 }
