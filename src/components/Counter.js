148956c3e4d79354a11e8d8a229763b711666fc0
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-22 18:57:12 +0300

    completed counter app

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,14 @@
 // bilgi => indirilen react eklentisi sayesinde rfce + tab yapınca react otomatik olarak geliyor.
 import React from 'react'
+import { useState } from "react";
 
 function Counter() {
+    const [count, setCount] = useState(0);
     return (
         <div>
-            
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
         </div>
     )
 }
