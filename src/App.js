f621c462409dfec8671fa1ab2590aa9af7cc322d
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-24 16:59:03 +0300

    unnecessary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import ThirdState from './components/ThirdState';
 
 function App() {
 
   return (
     <div className="App">
+
     </div>
   );
 }
