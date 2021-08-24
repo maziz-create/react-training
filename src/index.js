5312177fb46afbe2454f8619b0c4e6d6e0b0ec95
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-24 11:36:32 +0300

    Existing content deleted because new content will be added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/Counter.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
