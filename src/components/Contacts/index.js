71dcc5d4ce948cd0489c95cc336feb93e80324f0
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-25 11:23:14 +0300

    setted initial values of contacts, getting contacts from input via setContacts in Form Component. Contacts passed from Contacts Component to List Component

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React from "react";
 import List from './List';
 import Form from './Form';
+import {} from './styles.css';
+
+import { useState, useEffect } from 'react'
 
 function Contacts() {
+
+    const [contacts, setContacts] = useState([
+        {
+            fullname: "defaultKayit1",
+            phone_number: "1234123"
+        },
+        {
+            fullname: "defaultKayit2",
+            phone_number: "1255123"
+        },
+        {
+            fullname:"aziz",
+            phone_number:"1234123"
+        },
+        {
+            fullname:"feyza",
+            phone_number:"1234123"
+        },
+        {
+            fullname:"esra",
+            phone_number:"1234123"
+        }
+    ]);
+
+    useEffect(() => {
+
+    }, [contacts])
+
     return (
-        <div>
-            <List />
-            <Form />
+        <div id="container">
+            {/* Form'dan aldığımız verileri list'e gönderiyoruz. */}
+            <List contacts={contacts} />
+            <Form addContact={setContacts} contacts={contacts} />
         </div>
     )
 }
