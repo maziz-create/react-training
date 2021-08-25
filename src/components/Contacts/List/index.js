751b8d294c1c819128fa42b22396b09a884f0957
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-25 11:39:21 +0300

    Contacts received from Contacts Component via Form Component are listed to the screen in List Component. Added filtering in list. If values getting from filter input exist in contacts, it is listed in screen. Added related descriptions

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState } from "react";
 
-function List() {
+function List({ contacts }) {
+
+    const [filterText, setFilterText] = useState('');
+
+    const filtered = contacts.filter((item) => {
+        // Object.keys => item'in keylerini veriyor yani fullname ve phone_number.
+        // some => herhangi birisi yani phone_number ya da fullname
+        return Object.keys(item).some((key) =>
+            item[key] //item.fullname ya da item.phone_number
+                .toString()
+                .toLowerCase()
+                .includes(filterText.toLocaleLowerCase())
+        )
+    })
+
+    // console.log(filtered); //test
+
     return (
         <div>
-            Contact List
+            <input
+                placeholder="filter contact"
+                value={filterText}
+                onChange={(e) => setFilterText(e.target.value)}
+            />
+            <ul className="list">
+                {
+                    filtered.map((contact, index) => (
+                        <li key={index} >{contact.fullname}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
