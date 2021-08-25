3ee027dbcac3279ee36388a8f856c00b5b0f9ed1
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-25 11:31:49 +0300

    Values from input event setted to form state. Added related function. Added some little validation into onSubmit method.

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
-function Form() {
+// formu submit ettikten sonra değerlerini sıfırlıyoruz.
+const initialFormValues = { fullname: "", phone_number: "" };
+
+function Form({ addContact, contacts }) {
+
+    const [form, setForm] = useState(initialFormValues);
+
+    const onChangeInput = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault(); //form submit olduğunda sayfa yenilenmesini önler.
+
+        // validation
+        if (form.fullname === '' || form.phone_number === '') {
+            console.log("An unexpected error occured");
+            return false;
+        }
+
+        addContact([...contacts, form]);
+    }
+
+    useEffect(() => {
+        setForm(initialFormValues);
+    }, [contacts]);
+
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={onSubmit}>
+            <div>
+                <input
+                    placeholder="Full Name"
+                    value={form.fullname}
+                    name="fullname"
+                    onChange={onChangeInput} />
+            </div>
+
+            <div>
+                <input
+                    placeholder="Phone Number"
+                    value={form.phone_number}
+                    name="phone_number"
+                    onChange={onChangeInput} />
+            </div>
+
+            <br />
+
+            <button>Add</button>
+
+        </form>
     )
 }
 
